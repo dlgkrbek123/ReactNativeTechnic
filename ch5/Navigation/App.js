b0b6f05480202a3648fd637b35d29ddfaa622692
hagus <dlgkrbek123@gmail.com>
2021-12-07 22:23:55 +0900

    5단원 하단탭 내비게이터 구현 완료

--- ch5/Navigation/App.js
+++ ch5/Navigation/App.js
@@ -1,59 +1,70 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {View, Text, Button} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
-const HomeScreen = ({navigation}) => {
-  return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        title="Drawer 열기"
-        onPress={() => {
-          navigation.openDrawer();
-        }}
-      />
-      <Button
-        title="Setting 열기"
-        onPress={() => navigation.navigate('Setting')}
-      />
-    </View>
-  );
+const HomeScreen = ({}) => {
+  return <Text>Home</Text>;
 };
-
-const SettingScreen = ({navigation}) => {
-  return (
-    <View>
-      <Text>Setting</Text>
-      <Button title="뒤로가기" onPress={() => navigation.goBack()} />
-    </View>
-  );
+const SearchScreen = ({}) => {
+  return <Text>Search</Text>;
+};
+const NotificationScreen = () => {
+  return <Text>Notification</Text>;
+};
+const MessgaeScreen = () => {
+  return <Text>Message</Text>;
 };
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Home"
-        drawerPosition="left"
-        backBehavior="history"
-        screenOptions={{
-          drawerActiveBackgroundColor: '#fb8c00',
-          drawerActiveTintColor: 'white',
-        }}>
-        <Drawer.Screen
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: '홈'}}
+          options={{
+            title: '홈',
+            tabBarIcon: ({color, size}) => {
+              return <Icon name="home" color={color} size={size} />;
+            },
+          }}
         />
-        <Drawer.Screen
-          name="Setting"
-          component={SettingScreen}
-          options={{title: '설정'}}
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            title: '검색',
+            tabBarIcon: ({color, size}) => {
+              return <Icon name="search" color={color} size={size} />;
+            },
+          }}
         />
-      </Drawer.Navigator>
+        <Tab.Screen
+          name="Notification"
+          component={NotificationScreen}
+          options={{
+            title: '알림',
+            tabBarIcon: ({color, size}) => {
+              return <Icon name="notifications" color={color} size={size} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Message"
+          component={MessgaeScreen}
+          options={{
+            title: '메시지',
+            tabBarIcon: ({color, size}) => {
+              return <Icon name="message" color={color} size={size} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
