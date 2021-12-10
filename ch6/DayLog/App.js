ccc7e6d4820f911cfced55f15e3820b265316760
hagus <dlgkrbek123@gmail.com>
2021-12-11 01:08:39 +0900

    6단원 context, render props 학습

--- ch6/DayLog/App.js
+++ ch6/DayLog/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import LogContext from './contexts/LogContext';
 import RootStack from './screens/RootStack';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <LogContext.Provider value="안녕하세요">
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </LogContext.Provider>
   );
 };
 
