01b85acfd9846dc938634d6230077eccd307e12d
hagus <dlgkrbek123@gmail.com>
2021-12-16 23:45:02 +0900

    7단원 캘린더 피드 앱 AsyncStorage 적용

--- ch6_7/DayLog/contexts/LogContext.js
+++ ch6_7/DayLog/contexts/LogContext.js
@@ -1,9 +1,11 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useRef, useEffect, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
+import logsStorage from '../storages/logsStorage';
 
 const LogContext = createContext([]);
 
 export const LogContextProvidr = ({children}) => {
+  const initialLogsRef = useRef(null);
   const [logs, setLogs] = useState([]);
 
   const onCreate = ({title, body, date}) => {
@@ -22,6 +24,20 @@ export const LogContextProvidr = ({children}) => {
   const onRemove = id => {
     setLogs(logs.filter(log => log.id !== id));
   };
+
+  useEffect(() => {
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (initialLogsRef.current !== logs) logsStorage.set(logs);
+  }, [logs]);
 
   return (
     <LogContext.Provider value={{logs, onCreate, onModify, onRemove}}>
